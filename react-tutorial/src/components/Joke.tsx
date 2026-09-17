6302e6362aeded9d46432e3a3636ea3f0cbbfcbe
Dydex <supremeajala@gmail.com>
2026-09-17 19:21:48 +0100

    chore: remove react-tutorial

--- react-tutorial/src/components/Joke.tsx
+++
@@ -1,9 +0,0 @@
-export default function Joke(props: any) {
-    return (
-        <>
-        {props.setup && <p className="setup">Setup: {props.setup}</p> }
-        <p className="punchline">Punchline: {props.punchline} </p>
-        <hr />
-        </>
-    )
-} 
